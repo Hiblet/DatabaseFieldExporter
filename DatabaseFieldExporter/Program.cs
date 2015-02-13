ec34c963cc3d0bdca00ff1d2740c10e7813e1bf3
Hiblet <stevehibbert@zopa.com>
2015-02-13 11:01:42 +0000

    CHANGED: Program.cs  - Rudimentary starting comments.
    CHANGED: app.config
     - File changes here should be being ignored.

--- DatabaseFieldExporter/Program.cs
+++ DatabaseFieldExporter/Program.cs
@@ -7,6 +7,8 @@ namespace DatabaseFieldExporter
 {
     class Program
     {
+        private static log4net.ILog logger = log4net.LogManager.GetLogger("Program");
+
         static void Main(string[] args)
         {
             var prefix = "Main() - ";
@@ -15,8 +17,15 @@ namespace DatabaseFieldExporter
             // Start LOG4NET
             log4net.Config.XmlConfigurator.Configure();
 
+            logger.Debug(prefix + "Entering.");
 
 
+
+
+            Console.WriteLine("Press RETURN to exit...");
+            Console.ReadLine();
+
+            logger.Debug(prefix + "Exiting.");
         }
     }
 }
