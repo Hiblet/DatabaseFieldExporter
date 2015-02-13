bc6ae0eabbdf88e4788b5a47d4510753d95af40e
Hiblet <stevehibbert@zopa.com>
2015-02-13 10:25:16 +0000

    First Commit

---
+++ DatabaseFieldExporter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DatabaseFieldExporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prefix = "Main() - ";
+
+            ///////////////////////////////////////////////////////////////////
+            // Start LOG4NET
+            log4net.Config.XmlConfigurator.Configure();
+
+
+
+        }
+    }
+}
